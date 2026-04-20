506a37f2a7d91929770abf3f4b149cae3918c7da
RIBM <ribm205@gmail.com>
2026-04-19 23:18:10 -0600

    created TOF4Walls library to handle retreaving in formation from all TOFS

---
+++ Main/lib/TOF4Walls/TOF4Walls.h
@@ -0,0 +1,54 @@
+#ifndef TOF4WALLS_H
+#define TOF4WALLS_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53l8cx.h>
+
+class TOF4Walls {
+public:
+    enum Side : uint8_t {
+        FRONT = 0,
+        BACK  = 1,
+        LEFT  = 2,
+        RIGHT = 3
+    };
+
+    TOF4Walls(TwoWire& wire,
+              int lpnFront,
+              int lpnBack,
+              int lpnLeft,
+              int lpnRight);
+
+    bool begin(uint8_t freqHz = 60);
+    void update();
+
+    int16_t getDistance(Side side) const;
+    bool hasFreshData(Side side) const;
+    uint8_t getLastStatus(Side side) const;
+
+private:
+    static const uint8_t CENTRAL_ZONES[4];
+
+    TwoWire* _wire;
+    int _lpnPins[4];
+
+    VL53L8CX _front;
+    VL53L8CX _back;
+    VL53L8CX _left;
+    VL53L8CX _right;
+
+    int16_t _distances[4];
+    bool _fresh[4];
+    uint8_t _lastStatus[4];
+
+    bool initOne(VL53L8CX& sensor, int lpnPin, uint8_t newAddress, uint8_t freqHz);
+    void updateOne(VL53L8CX& sensor, uint8_t index);
+
+    int16_t computeWallDistance(const VL53L8CX_ResultsData& results, uint8_t& chosenStatus) const;
+
+    static bool isUsableStatus(uint8_t status);
+    static int16_t medianInt16(int16_t* values, uint8_t count);
+};
+
+#endif
--- README.md
+++
@@ -1,2 +0,0 @@
-# chaBots -WRO Future Engineers 2026
-Source code and documentation from team chaBots Tuneados competing in the World Robot Olympiad Future Engineers 2026. 
